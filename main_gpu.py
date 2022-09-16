263daa71eecc2a9bc9a44ec2972686af656c996a
Yiming Shao <shao@my.yorku.ca>
2022-09-16 16:22:43 -0400

    Update main_gpu.py

--- main_gpu.py
+++ main_gpu.py
@@ -187,7 +187,7 @@ def train_gpu(args):
     STATIC_SIZE = GPUS_PER_MACHINE * MACHINES_PER_RACK * RACKS_PER_CLUSTER + 1
 
     DYNAMIC_SIZE = 2 # (load, demand)
-    NUM_SAMPLES = 1000
+    NUM_SAMPLES = 100
     logger = Logger(f'./logs/test')
 
     train_data = GpuAssignmentDataset(num_samples=NUM_SAMPLES,
