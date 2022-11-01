805f38c80587d06933e72019be73bf9f10fcad1b
Yiming Shao <shao@my.yorku.ca>
2022-10-31 23:14:05 -0400

    Update main_gpu.py

--- main_gpu.py
+++ main_gpu.py
@@ -238,7 +238,7 @@ if __name__ == '__main__':
     parser.add_argument('--max_grad_norm', default=1, type=float)
     parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--hidden', dest='hidden_size', default=1024, type=int)
-    parser.add_argument('--dropout', default=0.4, type=float)
+    parser.add_argument('--dropout', default=0.8, type=float)
     parser.add_argument('--layers', dest='num_layers', default=3, type=int)
     parser.add_argument('--train-size',default=10000, type=int)
     parser.add_argument('--valid-size', default=1000, type=int)
